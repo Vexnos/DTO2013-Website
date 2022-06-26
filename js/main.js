4fe8045681ac9c3dcd981649858006f89d63387b
Vexnos <88638499+Vexnos@users.noreply.github.com>
2022-06-27 09:50:22 +1200

    Hamburger menu

--- js/main.js
+++ js/main.js
@@ -11,12 +11,20 @@ const modalImg = document.querySelector(".modalImg");
 const modalTxt = document.querySelector(".modalTxt");
 
 images.forEach(image => {
-  image.addEventListener("click", () => {
-    modalImg.src = image.src;
-    modal.classList.add("appear");
-  });
+    image.addEventListener("click", () => {
+        modalImg.src = image.src;
+        modal.classList.add("appear");
+    });
 
-  modalImg.addEventListener("click", () => {
-    modal.classList.remove("appear");
-  });
+    modalImg.addEventListener("click", () => {
+        modal.classList.remove("appear");
+    });
+});
+
+// Hamburger Menu
+const toggle = document.querySelector(".toggle-btn");
+const nav = document.querySelector(".links");
+
+toggle.addEventListener('click', () => {
+    nav.classList.toggle('clicked');
 });
