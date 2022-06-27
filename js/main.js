0bf3606b026b8cbd64a78d21b3c8cbe99d03cb78
Vexnos <88638499+Vexnos@users.noreply.github.com>
2022-06-27 14:24:44 +1200

    Commenting!

--- js/main.js
+++ js/main.js
@@ -5,26 +5,26 @@ Author: Daniel Lagesse
 Date: 2022-06-23
 ----------------------*/
 // Modal Iamges
-const images = document.querySelectorAll(".images");
-const modal = document.querySelector(".modal");
-const modalImg = document.querySelector(".modalImg");
-const modalTxt = document.querySelector(".modalTxt");
+const images = document.querySelectorAll(".images"); // Target all images with the "images" class
+const modal = document.querySelector(".modal"); // Target the modal
+const modalImg = document.querySelector(".modalImg"); // Target the modal image
+const modalTxt = document.querySelector(".modalTxt"); // Target the modal text
 
-images.forEach(image => {
-    image.addEventListener("click", () => {
-        modalImg.src = image.src;
-        modal.classList.add("appear");
+images.forEach(image => { // Loops for each image with the "images" class
+    image.addEventListener("click", () => { // Event listener for when the user clicks on an image
+        modalImg.src = image.src; // Send the clicked image to the modal image
+        modal.classList.add("appear"); // Add ".appear" to the classlist with its own styling that enlarges the image
     });
 
     modalImg.addEventListener("click", () => {
-        modal.classList.remove("appear");
+        modal.classList.remove("appear"); // Remove ".appear" from the classlist that shrinks the image
     });
 });
 
 // Hamburger Menu
-const toggle = document.querySelector(".toggle-btn");
-const nav = document.querySelector(".links");
+const toggle = document.querySelector(".toggle-btn"); // Targets the Hamburger Menu button
+const nav = document.querySelector(".links"); // Targets the Navigation bar links
 
-toggle.addEventListener('click', () => {
-    nav.classList.toggle('clicked');
+toggle.addEventListener('click', () => { // Event listener for when the user clicks/taps on the hamburger menu
+    nav.classList.toggle('clicked'); // Adds a "clicked" to the classlist with its own styling that makes the navbar links appear or disappear
 });
